5a413debcb02885bae19de91d6fa622027d500c4
Mazen <mazen_sehgal@hotmail.com>
2015-12-02 15:49:27 +0000

    Completed with some tests

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
  resources :pins
  get 'about'   => 'pages#about'
+ get 'shop'    => 'pins#index'
  get 'contact' => 'pages#contact'
  post 'request_contact', to: 'pages#request_contact'
  
